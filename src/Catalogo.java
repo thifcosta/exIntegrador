bf2fe880564c5c76d0d9530d2f71ad2474940f86
Thiago Fogagnoli Costa <thiferas@gmail.com>
2019-05-18 14:24:19 -0300

    correcao de erros durante user input

--- src/Catalogo.java
+++ src/Catalogo.java
@@ -1,4 +1,8 @@
+import java.text.DecimalFormat;
+import java.text.ParseException;
 import java.util.*;
+
+import static java.lang.Integer.parseInt;
 
 public class Catalogo {
 
@@ -29,13 +33,12 @@ public class Catalogo {
         estoque.put(livro10.getCodigo(), livro10);
     }
 
-    /////////////////////////////////////////////////////// MÉTODOS SEM INTERAÇÃO DO USUÁRIO
+    /////////////////////////////////////////////////////// MÉTODOS
 
-    public void cadastrarLivro(String codigo, String titulo, String autor, Integer anoLancamento, String isbn, Integer qtdEstoque, float preco){
-        Livro criarLivro = new Livro(codigo, titulo, autor, anoLancamento, isbn, qtdEstoque, preco);
-        estoque.put(criarLivro.getCodigo(), criarLivro);
-    }
+    ////////// Método para formatar os valores em R$ dos livros em duas casas decimais;
+    DecimalFormat numberFormat = new DecimalFormat("#.00");
 
+    ////////// Método para consultar o mapa estoque e apresentar o objeto livro através do seu código;
     public void consultarLivroPorCodigo(String codigo){
         Livro consultaAtual = estoque.get(codigo);
         System.out.println("\n****************************************\n");
@@ -45,27 +48,28 @@ public class Catalogo {
                 "Ano de Lançamento: "+consultaAtual.getAnoLancamento()+"\n" +
                 "ISBN: "+consultaAtual.getIsbn()+"\n" +
                 "Quantidade em Estoque: "+consultaAtual.getQtdEstoque()+"\n" +
-                "Preço: R$ "+consultaAtual.getPreco()+"\n\n" +
+                "Preço: R$ "+numberFormat.format(consultaAtual.getPreco()).toString()+"\n\n" +
                 "****************************************");
     }
 
+    ////////// Teste de formatação de texto, para apresentar as informações de forma visualmente mais agradável,
     public void listaDeLivros(){
 
-        System.out.println("|---------------|---------------|---------------|---------------|---------------|---------------|---------------|\n"+
-                "|                                           LISTA COMPLETA DE ESTOQUE                                           |\n"+
-                "|---------------|---------------|---------------|---------------|---------------|---------------|---------------|\n"+
-                "|CÓDIGO         |TÍTULO         |AUTOR          |LANÇAMENTO     |ISBN           |ESTOQUE        |PREÇO          |\n"+
-                "|---------------|---------------|---------------|---------------|---------------|---------------|---------------|");
+        System.out.println("|--------------------|--------------------|--------------------|--------------------|--------------------|--------------------|--------------------|\n"+
+                "|                                                            LISTA COMPLETA DE ESTOQUE                                                             |\n"+
+                "|--------------------|--------------------|--------------------|--------------------|--------------------|--------------------|--------------------|\n"+
+                "|CÓDIGO              |TÍTULO              |AUTOR               |LANÇAMENTO          |ISBN                |ESTOQUE             |PREÇO               |\n"+
+                "|--------------------|--------------------|--------------------|--------------------|--------------------|--------------------|--------------------|");
         for (String code :estoque.keySet()){
             Livro livro = estoque.get(code);
             catalogoFormatado(livro);
         }
-        System.out.println("|---------------|---------------|---------------|---------------|---------------|---------------|---------------|");
+        System.out.println("|--------------------|--------------------|--------------------|--------------------|--------------------|--------------------|--------------------|");
 
     }
 
+    ////////// Método usado em listaDeLivros(), para puxar as informações de cada livro e apresentá-las em uma linha, com espaçamento e formatação correta;
     public void catalogoFormatado(Livro liv){
-
         List<String> listaDeAtributos = new ArrayList<>();
 
         String codigo = liv.getCodigo();
@@ -74,7 +78,7 @@ public class Catalogo {
         String anoLancamento = liv.getAnoLancamento().toString();
         String isbn = liv.getIsbn();
         String qtdEstoque = liv.getQtdEstoque().toString();
-        String preco = "R$ "+Float.toString(liv.getPreco());
+        String preco = "R$ "+ numberFormat.format(liv.getPreco()).toString();
 
         listaDeAtributos.add(codigo);
         listaDeAtributos.add(titulo);
@@ -87,10 +91,10 @@ public class Catalogo {
         String linhaFormatada = "";
 
         for (String string :listaDeAtributos){
-            if(string.length()>12){
-                string = string.substring(0,12)+"...";
+            if(string.length()>17){
+                string = string.substring(0,17)+"...";
             } else {
-                int theLen = 15-(string.length());
+                int theLen = 20-(string.length());
                 for(int i = 0; i<theLen; i++){
                     string = string+" ";
                 }
@@ -103,7 +107,50 @@ public class Catalogo {
 
     }
 
+    ////////// Método usado em userCadastrarLivro(), para criar um novo scanner que recebe uma String e validar se não está em branco;
+    private String validString(String userInput){
+        Scanner scn = new Scanner (System.in);
+        String theStr = scn.nextLine();
+        while(theStr.length()==0||theStr.substring(0,1).equals(" ")){
+            System.out.println("O campo \""+userInput+"\" não pode iniciar com um espaço em branco. Digite novamente:");
+            theStr = scn.nextLine();
+        }
+        return theStr;
+    }
 
+    ////////// Método usado em userCadastrarLivro(), para criar um novo scanner que recebe um Integer e validar se não está em branco;
+    private Integer validInt(String userInput) {
+        boolean isParse = false;
+        String str = "";
+        while (!isParse) {
+            str = validString(userInput);
+            try {
+                Integer.parseInt(str);
+                isParse = true;
+            } catch (Exception e) {
+                System.out.println("O campo \"" + userInput + "\" só recebe números. Digite novamente:");
+            }
+        }
+        return Integer.parseInt(str);
+    }
+
+    ////////// Método usado em userCadastrarLivro(), para criar um novo scanner que recebe um Float e validar se não está em branco;
+    private float validFloat(String userInput) {
+        boolean isParse = false;
+        String str = "";
+        while (!isParse) {
+            str = validString(userInput);
+            try {
+                Float.parseFloat(str);
+                isParse = true;
+            } catch (Exception e) {
+                System.out.println("O campo \"" + userInput + "\" só recebe números. Digite novamente:");
+            }
+        }
+        return Float.parseFloat(str);
+    }
+
+    ////////// Método utilizado para pedir ao usuário informações em sequência, para criar um novo livro e adicioná-lo ao mapa estoque;
     public void userCadastrarLivro(){
         String codigo;
         String titulo;
@@ -115,27 +162,25 @@ public class Catalogo {
 
         Scanner scn = new Scanner( System.in );
         System.out.println("Vamos cadastrar um novo livro!\nDigite um novo código: ");
-        codigo = scn.nextLine();
-
+        codigo = validString("Código");
         for (String code : estoque.keySet()){
-            while (codigo.equals(code)){
+            while(codigo.equals(code)) {
                 System.out.println("Código já existente. Digite um código único:");
-                codigo = scn.nextLine();
+                codigo = validString("Código");
             }
         }
-
         System.out.println("Digite o título do Livro:");
-        titulo = scn.nextLine();
+        titulo = validString("Título");
         System.out.println("Digite o nome do Autor:");
-        autor = scn.nextLine();
+        autor = validString("Autor");
         System.out.println("Digite o ano de lançameto:");
-        anoLancamento = scn.nextInt();
+        anoLancamento = validInt("Ano de Lançamento");
         System.out.println("Digite o ISBN:");
-        isbn = scn.next();
+        isbn = validString("ISBN");
         System.out.println("Qual a quantidade deste novo livro em estoque?");
-        qtdEstoque = scn.nextInt();
+        qtdEstoque = validInt("Quantidade de Estoque");
         System.out.println("Qual o preço deste livro?");
-        preco = scn.nextFloat();
+        preco = validFloat("Preço");
 
         Livro criarLivro = new Livro(codigo, titulo, autor, anoLancamento, isbn, qtdEstoque, preco);
         estoque.put(criarLivro.getCodigo(), criarLivro);
@@ -145,6 +190,7 @@ public class Catalogo {
         consultarLivroPorCodigo(criarLivro.getCodigo());
     }
 
+    ////////// Método utilizado para pedir um código ao usuário e apresentar as informações do livro correspondente;
     public void userConsultarLivroPorCodigo(){
         System.out.println("Para fazer uma nova busca, digite o código do livro:");
         String codigo;
@@ -152,25 +198,17 @@ public class Catalogo {
         Scanner scn = new Scanner (System.in);
 
         while (livro == null) {
-            codigo = scn.next();
+            codigo = scn.nextLine();
             livro = estoque.get(codigo);
             if (livro == null) {
                 System.out.println("Este código não existe. Digite novamente: ");
             } else {
-                System.out.println("****************************************\n");
-                System.out.println("Resultado da busca:\n");
-                System.out.println("Código: "+livro.getCodigo());
-                System.out.println("Título: "+livro.getTitulo()+"\n" +
-                        "Autor: "+livro.getAutor()+"\n" +
-                        "Ano de Lançamento: "+livro.getAnoLancamento()+"\n" +
-                        "ISBN: "+livro.getIsbn()+"\n" +
-                        "Quantidade em Estoque: "+livro.getQtdEstoque()+"\n" +
-                        "Preço: R$ "+livro.getPreco()+"\n\n" +
-                        "****************************************");
+                consultarLivroPorCodigo(livro.getCodigo());
             }
         }
     }
 
+    ////////// Método usado para efetuar vendas, confirmando a quantidade, valor e alterando o estoque;
     public void userEfetuarVendaPorCodigo(){
 
         Scanner scn = new Scanner( System.in );
@@ -180,29 +218,28 @@ public class Catalogo {
         System.out.println("Digite o código do livro que deseja vender: ");
 
         while (livro == null) {
-            String codigo = scn.next();
+            String codigo = scn.nextLine();
             livro = estoque.get(codigo);
             if (livro == null) {
                 System.out.println("Este código não existe. Digite novamente: ");
             } else {
                 Integer estoqueAtual = livro.getQtdEstoque();
-                System.out.println("O livro selecionado foi: "+livro.getTitulo()+", escrito por: "+livro.getAutor()+".");
-                System.out.println("Temos "+livro.getQtdEstoque()+" unidades deste livro em estoque.\n");
-                System.out.println("Quantas unidades serão vendidas?");
+                System.out.println("\nO livro selecionado foi:\n\n****************************************\nTítulo: "+livro.getTitulo()+";\nAutor : "+livro.getAutor()+".\n****************************************\n");
+                System.out.println("Temos "+livro.getQtdEstoque()+" unidades deste livro em estoque.\nQuantas unidades serão vendidas?");
                 Integer vendaAtual = scn.nextInt();
                 while (estoqueAtual < vendaAtual) {
                     System.out.println("Temos apenas " + livro.getQtdEstoque() + " unidades deste livro :(\nDigite uma nova quantidade:");
                     vendaAtual = scn.nextInt();
                 }
 
-                System.out.println("O total da venda será de R$: "+vendaAtual*livro.getPreco()+"\nPara confirmar digite \"1\", cancelar digite \"2\":");
+                System.out.println("\nO total da venda será de:\nR$ "+numberFormat.format(vendaAtual*livro.getPreco()).toString()+"\n\nPara confirmar digite \"1\", cancelar digite \"2\":");
                 boolean validate = false;
                 while(!validate){
                     String resp = scn.next();
                     switch(resp){
                         case "1":
                             livro.setQtdEstoque(estoqueAtual - vendaAtual);
-                            System.out.println("****************************************\n\nVENDA EFETUADA COM SUCESSO!\n\nConfira abaixo as informações atualizadas:");
+                            System.out.println("****************************************\n\nVENDA EFETUADA COM SUCESSO!\nConfira abaixo as informações atualizadas:");
                             consultarLivroPorCodigo(livro.getCodigo());
                             validate = true;
                             break;
